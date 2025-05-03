0d2ed03f93b7909b9b65062dcced54725731f562
markfran11 <relevomarky@gmail.com>
2025-05-03 16:47:12 +0800

    Lab4

---
+++ Relevo_MarkFrancis_Lab_4.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+string karaoke_queue[100];
+
+int front = 0;
+int rear = -1;
+int max_queue = 10;
+
+bool isFull(){
+    if(rear == max_queue -1){
+        return true;
+    }
+    
+    return false;
+}
+
+bool isEmpty(){
+    if(rear == -1){
+        return true;
+    }
+    
+    return false;
+}
+
+string enqueue(string song){
+    if(isFull()){
+        return "The queue is full. Try again later";
+    }
+    
+    karaoke_queue[++rear] = song;
+    return "A new song has been added in the queue";
+}
+
+string dequeue(){
+    if(isEmpty()){
+        return "There is no song in the queue. Try adding first";
+    }
+    
+    string song = karaoke_queue[front];
+    
+    for(int i = 1; i <= rear; i++){
+        karaoke_queue[i-1] = karaoke_queue[i];
+    }
+    
+    rear--;
+    return song + " has been deleted the queue";
+}
+
+int main(){
+    
+    cout << enqueue ("Beer - Itchyworms") << endl;
+    cout << enqueue ("Ang Huling El Bimbo - Eraserheads") << endl;
+    cout << enqueue ("Jopay - Mayonnaise") << endl;
+    
+    cout << dequeue () << endl;
+    cout << dequeue () << endl;
+    
+    return 0;
+}
